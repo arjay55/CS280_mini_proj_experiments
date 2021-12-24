e08c51ef32c58783bdbc829cf1e7ce1b2c3a794a
Elton Mark Lanuza <96604999+eltonmlanuza@users.noreply.github.com>
2021-12-24 22:25:52 +0800

    ngboost test
    boston housing dataset pa lang. di pa yung actual

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from ngboost import NGBRegressor
+
+from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
 from pandas import read_csv
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -10,13 +17,33 @@ series_value = series.values
 series_index = series.index
 print("series_value: ", series_value)
 print("series_index: ", series_index)
-#x, y = series_value[:, :-1], series_value[:, -1]
-#print("x: ", x)
-#print("y: ", y)
+# x, y = series_value[:, :-1], series_value[:, -1]
+# print("x: ", x)
+# print("y: ", y)
 
 plt.plot(series_index, series_value, '--bo', label='Training Average Sum of Squared Errors')
 #plt.show()
-print("last na")
-print("\nseries:\n", series)
+#print("\nseries:\n", series)
 #series.plot()
+
 plt.show()
+print("load boston: ", np.array(load_boston()))
+
+X, Y = load_boston(return_X_y=True)
+print("X: ", X)
+print("Y: ", Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+ngb = NGBRegressor().fit(X_train, Y_train)
+Y_preds = ngb.predict(X_test)
+Y_dists = ngb.pred_dist(X_test)
+
+# test Mean Squared Error
+test_MSE = mean_squared_error(Y_preds, Y_test)
+print('Test MSE', test_MSE)
+
+# test Negative Log Likelihood
+test_NLL = -Y_dists.logpdf(Y_test).mean()
+print('Test NLL', test_NLL)
+
+
